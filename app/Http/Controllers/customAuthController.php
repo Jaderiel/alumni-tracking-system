172e316cfd40a34d5d6b4e427ac436c92357fff1
Jaderiel <jabuela22@gmail.com>
2024-03-10 10:08:20 +0800

    add 2 functions login and registration

--- app/Http/Controllers/customAuthController.php
+++ app/Http/Controllers/customAuthController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class customAuthController extends Controller
 {
-    //
+    public function login() {
+        return "Login";
+    }
+
+    public function registration() {
+        return "Register";
+    }
 }
